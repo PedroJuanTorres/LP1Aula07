9db23625b5c0c5a7d00eee840b9bdfab332ba1c7
Pedro <pedrojuantorres.info@gmail.com>
2021-04-21 11:20:22 +0100

    Upgrading Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,6 +2,15 @@ namespace MyGame
 {
     class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        //Construtor
+        public Enemy(string name){
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
