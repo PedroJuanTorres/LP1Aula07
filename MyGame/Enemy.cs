40b34ea7d92c902593f9f25d5e0cd1956a32be93
Pedro <pedrojuantorres.info@gmail.com>
2021-04-21 11:42:13 +0100

    Adding GetHealth e GetShield

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -16,6 +16,12 @@ namespace MyGame
             return name;
         }
 
+        public float GetHealth(){
+            return health;
+        }
+        public float GetShield(){
+            return shield;
+        }
         public void TakeDamage(float damage){
             shield -= damage;
             if(shield < 0){
